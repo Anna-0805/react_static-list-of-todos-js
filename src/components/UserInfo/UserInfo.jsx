8b5cd2f370d16a681c58863a62df244990832e85
Anna Onyshchenko <auonishchenko.com>
2025-04-19 14:47:26 +0300

    add task solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,11 @@
-// Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+import './UserInfo.scss';
+
+export const UserInfo = ({ users }) => (
+  <>
+    {users.map(user => (
+      <a className="UserInfo" href={user.email}>
+        {user.name}
+      </a>
+    ))}
+  </>
+);
